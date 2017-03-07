31f42668a37dffa126563d7e742b87c6668c0d2d
abarrak <a@abarrak.com>
2017-03-07 06:04:08 +0300

    fix: cards resources routing specs
    This commit resolves the issue with the routing rspec suite which was mainly due to missed segments
    in the url `/api/v1/` in addition to the needed :format param since rspec misses it form the router
    fallback default.

--- spec/routing/cards_routing_spec.rb
+++ spec/routing/cards_routing_spec.rb
@@ -1,29 +1,29 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe CardsController, type: :routing do
-#   describe "routing" do
-#     it "routes to #index" do
-#       expect(:get => "api/v1/cards").to route_to("cards#index")
-#     end
+RSpec.describe CardsController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/api/v1/cards").to route_to("cards#index", :format => :json)
+    end
 
-#     it "routes to #show" do
-#       expect(:get => "api/v1/cards/1").to route_to("cards#show", :id => "1")
-#     end
+    it "routes to #show" do
+      expect(:get => "/api/v1/cards/1").to route_to("cards#show", :id => "1", :format => :json)
+    end
 
-#     it "routes to #create" do
-#       expect(:post => "api/v1/cards").to route_to("cards#create")
-#     end
+    it "routes to #create" do
+      expect(:post => "/api/v1/cards").to route_to("cards#create", :format => :json)
+    end
 
-#     it "routes to #update via PUT" do
-#       expect(:put => "api/v1/cards/1").to route_to("cards#update", :id => "1")
-#     end
+    it "routes to #update via PUT" do
+      expect(:put => "/api/v1/cards/1").to route_to("cards#update", :id => "1", :format => :json)
+    end
 
-#     it "routes to #update via PATCH" do
-#       expect(:patch => "api/v1/cards/1").to route_to("cards#update", :id => "1")
-#     end
+    it "routes to #update via PATCH" do
+      expect(:patch => "/api/v1/cards/1").to route_to("cards#update", :id => "1", :format => :json)
+    end
 
-#     it "routes to #destroy" do
-#       expect(:delete => "api/v1/cards/1").to route_to("cards#destroy", :id => "1")
-#     end
-#   end
-# end
+    it "routes to #destroy" do
+      expect(:delete => "/api/v1/cards/1").to route_to("cards#destroy", :id => "1", :format => :json)
+    end
+  end
+end
